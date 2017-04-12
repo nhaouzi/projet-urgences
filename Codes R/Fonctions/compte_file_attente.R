ccbf45a7f1fc487b015b4090b7f67281a79454da
nhaouzi <noemiehaouzi@gmail.com>
2017-04-12 22:06:24 +0200

    ajout nb personne attente par classe iao

--- Codes R/Fonctions/compte_file_attente.R
+++ Codes R/Fonctions/compte_file_attente.R
@@ -7,5 +7,32 @@ compte_file_attente = function(TS_adm, TS_med,  evt) {
   return(nb_attente = nrow(en_attente))
 }
 
+compte_file_attente_par_iao = function(TS_adm, TS_med,  evt, tri_iao) {
+  # trier la base par date de ts.adm peut etre plus rapide
+  tab = data.table(adm = TS_adm,
+                   med = TS_med, 
+                   iao = tri_iao)
+  tmps_date_adm = difftime(evt, tab$adm, units = "secs") 
+  tmps_med_date = difftime(tab$med, evt, units = "secs")
+  en_attente = tab[tmps_date_adm > 0 & tmps_med_date > 0]
+  print(dim(en_attente))
+  if(nrow(en_attente)!=0) {
+    attente_iao1 = nrow(en_attente[iao ==1])
+    attente_iao2 = nrow(en_attente[iao ==2])
+    attente_iao3 = nrow(en_attente[iao ==3])
+    
+  }
+  else {
+    attente_iao1 <- 0
+    attente_iao2 <- 0
+    attente_iao3 <- 0
+  }
+  return(list(nb_attente = nrow(en_attente),
+              attente_iao1 = attente_iao1,
+              attente_iao2 = attente_iao2,
+              attente_iao3 = attente_iao3))
+}
+
+
 # tmp_iao = tmp[as.numeric(TS.iao) > as.numeric(evt)]
 # tmp_med = tmp[as.numeric(TS.med) > as.numeric(evt)]
