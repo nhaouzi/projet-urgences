e99d65df8201e229a5924ee55ee8a49953383d13
nhaouzi <noemiehaouzi@gmail.com>
2017-04-16 14:13:22 +0200

    creation nouvelles bases avec 2017

--- Codes R/Fonctions/compte_file_attente.R
+++ Codes R/Fonctions/compte_file_attente.R
@@ -42,7 +42,7 @@ moyenne_file_attente_par_iao = function(TS_adm, TS_med,  evt, tri_iao, fenetre_n
   #tmps_date_adm = difftime(evt, tab$adm, units = "min") 
   tmps_med_date = difftime(tab$med, evt, units = "hours")
   fenetre = tab[tmps_med_date > -fenetre_nb_heure & tmps_med_date < 0]
-  #print(fenetre)
+  print(dim(fenetre))
   if(nrow(fenetre)!=0) {
     attente_iao1 = mean(fenetre[iao ==1]$tps_attente)
     attente_iao2 = mean(fenetre[iao ==2]$tps_attente)
@@ -72,4 +72,127 @@ trouve_jour_fete = function(mois_fete, jour_fete, mois, jour) {
   tab[,jour_fete := ifelse(test = date_char %in% date_a_comparer, yes = 1, no = 0)]
   return(tab$jour_fete)
 }
+
+## exemple
+
+
+simule_observations_2017 = function(prob_joursemaine, prob_iao,
+                               prob_mois, prob_annee,
+                               prob_jourmois, prob_heure,
+                               prob_minute, 
+                               moyenne_hopital,
+                               nb_hopital,
+                               jour_fete, mois_fete,
+                               nom_hopital) {
+ 
+  jours = c("Dimanche", "Jeudi", "Lundi", "Mardi", "Mercredi", "Samedi", "Vendredi")
+  hopital = data.table(tri.iao = sample(1:3, size=nb_hopital, replace = T, 
+                                         prob = prob_iao),
+                        D.total = round(rexp(nb_hopital, 1/moyenne_hopital),0),
+                        jour_semaine = sample(jours, size = nb_hopital, 
+                                              replace = T, prob = prob_joursemaine),
+                        mois = sample(1:12, size=nb_hopital, replace = T,
+                                      prob = prob_mois),
+                        annee = rep(2017, nb_hopital),
+                        jour_mois = sample(1:31, size=nb_hopital, replace = T,
+                                           prob=prob_jourmois),
+                        heure_adm = sample(0:23, size=nb_hopital, replace = T,
+                                           prob_heure),
+                        min_adm = sample(0:59, size=nb_hopital, replace = T,
+                                         prob = prob_minute))
+  
+  hopital = as.data.frame(hopital)
+  for(j in c(4,6,7,8)) {
+    hopital[,j] = as.character(hopital[,j])
+    for(i in 1:nrow(hopital)) {
+      if(nchar(hopital[i,j])<2) hopital[i,j] = paste0("0", hopital[i,j]) 
+    }
+    print(j)
+  }
+  hopital = as.data.table(hopital)
+  
+  hopital[,adm := paste0(hopital$annee, "-", hopital$mois, "-", hopital$jour_mois, " ",
+                          hopital$heure_adm, ":", hopital$min_adm, ":00")]
+  
+  hopital[,TS.adm := strptime(adm, format = "%Y-%m-%d %H:%M:%S", tz = "Europe/Paris")]
+  hopital[,TS.med := TS.adm + minutes(D.total)]
+  hopital = na.omit(hopital, cols = c("TS.adm", "TS.med"))
+  hopital = hopital[order(hopital$TS.adm, decreasing = F)]
+  hopital[,adm:=NULL]
+  
+  
+  attente = sapply(hopital$TS.adm, function(x) 
+    compte_file_attente_par_iao(TS_adm = hopital$TS.adm, 
+                                TS_med = hopital$TS.med,
+                                evt = x, tri_iao = hopital$tri.iao))
+  
+  attente_iao1 = apply(attente, 2, function(x) x$attente_iao1)
+  attente_iao2 = apply(attente, 2, function(x) x$attente_iao2)
+  attente_iao3 = apply(attente, 2, function(x) x$attente_iao3)
+  attente_tot = apply(attente, 2, function(x) x$nb_attente)
+  
+  hopital[,nb_pers_attente := attente_tot]
+  hopital[,attente_iao1 := attente_iao1]
+  hopital[,attente_iao2 := attente_iao2]
+  hopital[,attente_iao3 := attente_iao3]
+  
+  hopital[,hopital := nom_hopital]
+  
+  moyenne_4 = sapply(hopital$TS.adm, function(x) 
+    moyenne_file_attente_par_iao(hopital$TS.adm, hopital$TS.med,  
+                                 x, 
+                                 hopital$tri.iao, 4))
+  
+  moyenne_2 = sapply(hopital$TS.adm, function(x) 
+    moyenne_file_attente_par_iao(hopital$TS.adm, hopital$TS.med,  
+                                 x, 
+                                 hopital$tri.iao, 2))
+  
+  moyenne_1 = sapply(hopital$TS.adm, function(x) 
+    moyenne_file_attente_par_iao(hopital$TS.adm, hopital$TS.med,  
+                                 x, 
+                                 hopital$tri.iao, 1))
+  
+  
+  moyenne_iao1 = apply(moyenne, 2, function(x) x$attente_iao1)
+  moyenne_iao2 = apply(moyenne, 2, function(x) x$attente_iao2)
+  moyenne_iao3 = apply(moyenne, 2, function(x) x$attente_iao3)
+  moyenne_tot = apply(moyenne, 2, function(x) x$nb_attente)
+  
+  hopital[,attente_moyenne := moyenne_tot]
+  hopital[,moyenne_iao1 := moyenne_iao1]
+  hopital[,moyenne_iao2 := moyenne_iao2]
+  hopital[,moyenne_iao3 := moyenne_iao3]
+  
+  for (col in c("moyenne_iao1", "moyenne_iao2", "moyenne_iao3", "attente_moyenne")) {
+    hopital[is.na(get(col)), (col) := -999]
+  }
+  
+  # hopital[,jour_fetes := trouve_jour_fete(jour_fete, mois_fete,
+  #                                          mois = hopital$mois,
+  #                                          jour = hopital$jour_mois)]
+
+  return(hopital)
+  
+}
+
+retourne_moyenne_attente = function(hopital, nb_heure) {
+  hopital[,TS_adm := as.POSIXct(TS_adm, tz="Europe/Paris", format="%Y-%m-%d %H:%M:%S")]
+  hopital[,TS_med := as.POSIXct(TS_med, tz="Europe/Paris", format="%Y-%m-%d %H:%M:%S")]
+  
+  moyenne = sapply(hopital$TS_adm, function(x) 
+    moyenne_file_attente_par_iao(hopital$TS_adm, hopital$TS_med,  
+                                 x, 
+                                 hopital$tri_iao, nb_heure))
+  
+  moyenne_iao1 = apply(moyenne, 2, function(x) x$attente_iao1)
+  moyenne_iao2 = apply(moyenne, 2, function(x) x$attente_iao2)
+  moyenne_iao3 = apply(moyenne, 2, function(x) x$attente_iao3)
+  moyenne_tot = apply(moyenne, 2, function(x) x$nb_attente)
+  
+  return(list(moyenne_iao1 = moyenne_iao1,
+              moyenne_iao2 = moyenne_iao2,
+              moyenne_iao3 = moyenne_iao3,
+              moyenne_tot = moyenne_tot))
+}  
 
